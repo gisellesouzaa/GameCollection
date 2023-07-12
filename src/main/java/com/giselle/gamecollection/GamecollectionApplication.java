7ac721841f54ece92b7d2b743757ea625a2f18a0
Giselle Souza <reallovetour@gmail.com>
2023-07-12 18:35:45 -0300

    Project created

---
+++ src/main/java/com/giselle/gamecollection/GamecollectionApplication.java
@@ -0,0 +1,13 @@
+package com.giselle.gamecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giselle/gamecollection/GamecollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giselle.gamecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
