292d14457f671483d1e5e90b25bf05463ded4030
Giselle Souza <reallovetour@gmail.com>
2023-07-15 13:49:24 -0300

    Get game by id

--- src/main/java/com/giselle/gamecollection/services/GameService.java
+++ src/main/java/com/giselle/gamecollection/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.giselle.gamecollection.dto.GameDTO;
 import com.giselle.gamecollection.dto.GameMinDTO;
 import com.giselle.gamecollection.entities.Game;
 import com.giselle.gamecollection.repositories.GameRepository;
@@ -18,5 +20,11 @@ public class GameService {
 	public List<GameMinDTO> findAll() {
 			List<Game> result = gameRepository.findAll();
 			return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
